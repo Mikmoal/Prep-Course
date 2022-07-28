f88df1f7c92893c2c881dc9d1dbe97058a94223a
Mikmoal <angeld7@live.com>
2022-07-28 12:26:42 -0500

    adds homework JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -59,11 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-  let str = palabras[0];
-  for ( let i = 1; i < palabras.length; i++) {
-    str += " " + palabras[i];
-  }
-  return str;
+  return palabras.join(' ');
 }
 
 function arrayContiene(array, elemento) {
@@ -121,17 +117,13 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  let productoArgs = 1;
-  if ( arguments.length < 1){
+  if(arguments.length < 1) 
     return 0;
-  } else if ( arguments.length == 1 ){
-    return arguments;
-  } else {
-    for ( let i = 0; i < arguments.length; i++) {
-      productoArgs *= arguments[i];
-    }
-    return productoArgs;
+  let total = 1;
+  for(let i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
   }
+  return total;
 }
 
 
@@ -165,9 +157,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  if ( n[0] === 9) {
-    return true;
-  } return false;
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
 }
 
 
@@ -175,13 +169,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  let x = arreglo[0];
-  for ( let i = 1; i < arreglo.length; i++) {
-    if ( arreglo[i] != x) {
-      return false;
+  for (let i =0 ; i < arreglo.length - 1; i++) {
+    if(arreglo[i] !== arreglo[i+1]){
+      return false
     }
   }
-  return true;
+  return true
 } 
 
 
@@ -193,9 +186,9 @@ function mesesDelAño(array) {
   let enero=0;
   let marzo=0;
   let noviembre=0;
-  const meses = [];
+  let meses = [];
   for ( let i = 0; i < array.length; i++) {
-    if ( array[i] == "Enero" || array[i] == "Marzo" || array[i] == "Noviembre") {
+    if ( array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
       switch (array[i]){
         case "Enero":
           enero = 1;
@@ -210,19 +203,20 @@ function mesesDelAño(array) {
       meses.push(array[i]);
     }
   }
-  if ( enero != 1 && marzo != 1 && noviembre != 1) {
+  if ( enero != 1 || marzo != 1 || noviembre != 1) {
     return "No se encontraron los meses pedidos";
   } else {
     return meses;
   }
 }
+console.log(mesesDelAño(["Marzo", "Diciembre", "Julio", "Noviembre"]));
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  const numerosMayores = [];
+  let numerosMayores = [];
   for ( let i = 0; i < array.length; i++) {
     if ( array[i] > 100) {
       numerosMayores.push(array[i]);
@@ -240,19 +234,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let array = [];
+  let suma = numero;
   let i = 0;
-  const numeros = [];
-  while (i < 10){
-    numero += 2;
-    numeros.push(numero);
-    if ( numero == i) {
-      break;
+  for(i; i < 10; i++) {
+    suma += 2;
+    if(suma === i) break;
+    else {
+      array.push(suma);
     }
   }
-  if ( numero == i) {
-    return "Se interrumpió la ejecución";
+  if (i < 10) {
+    return 'Se interrumpió la ejecución';
   }
-  return numeros;
+  else {
+      return array;
+  }
 }
 
 
@@ -263,17 +260,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-  let i = 0;
-  const numeros = [];
-  while (i < 10){
-    if ( i == 5) {
-      continue;
+  let array = [];
+  let suma = numero;
+  for(let i = 0; i < 10; i++) {
+    if(i !== 5) {
+      suma += 2;
+      array.push(suma);
     } else {
-      numero += 2;
-      numeros.push(numero);
+      continue;
     }
   }
-  return numeros;
+  return array;
 }
 
 
